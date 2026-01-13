3a34b2b00498019825d567c8ee17fcdb166acd74
Rejane2304 <rejane2304@gmail.com>
2026-01-13 19:07:45 +0100

    Corregir specPattern en Cypress: ahora detecta y ejecuta los tests E2E correctamente.

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000', 
     supportFile: false,
-    specPattern: 'frontend/cypress/e2e/**/*.cy.js'
+    specPattern: 'cypress/e2e/**/*.cy.js'
   }
 });
